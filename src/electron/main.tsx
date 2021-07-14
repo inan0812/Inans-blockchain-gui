dfa27634664375ce4e9975a37bc467a93cf310f1
inan0812 <dannymunoz07@gmail.com>
2021-07-13 22:36:23 -0400

    Port Change

--- src/electron/main.tsx
+++ src/electron/main.tsx
@@ -153,7 +153,7 @@ if (!handleSquirrelEvent()) {
 
       const startUrl =
         process.env.NODE_ENV === 'development'
-          ? 'http://localhost:30003'
+          ? 'http://localhost:3000'
           : url.format({
               pathname: path.join(__dirname, '/../renderer/index.html'),
               protocol: 'file:',
